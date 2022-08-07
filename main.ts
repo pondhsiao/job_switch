c1b94a87ad0d53d038e3a3b12edbf042314be106
pondhsiao <98569846+pondhsiao@users.noreply.github.com>
2022-08-07 09:59:47 +0800

    init

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 input.onGesture(Gesture.Shake, function () {
     String2 = 0
 })
@@ -9,6 +6,7 @@ String2 = 1
 basic.forever(function () {
     if (String2) {
         basic.showIcon(IconNames.Heart)
+        music.playTone(262, music.beat(BeatFraction.Double))
     } else {
         basic.showString("Hello!")
         String2 = 1
